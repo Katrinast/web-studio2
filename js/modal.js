a5c8466044e27372ee644cbc74364facc4233d5f
Katrinast <111525603+Katrinast@users.noreply.github.com>
2024-01-31 16:47:09 +0200

    Initial commit

---
+++ js/modal.js
@@ -0,0 +1,33 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.forEach((element) => {
+    element.addEventListener('click', toggleModal);
+  });
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  //* Закривання по кліку на бекдроп
+
+  refs.modal.addEventListener('click', removeModal);
+  function removeModal(e) {
+    if (e.target === e.currentTarget) {
+      refs.modal.classList.add('is-hidden');
+    }
+  }
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
+  document.querySelector(".section-hero-button").addEventListener("click", toggleOrderServiceModal);
+	document.querySelector(".modal-close-btn").addEventListener("click", toggleOrderServiceModal);
+
+	function toggleOrderServiceModal() {
+        document.querySelector(".backdrop").classList.toggle("is-open");
+        document.body.classList.toggle("no-scroll");
+	}
+})();
