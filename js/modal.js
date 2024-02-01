bfcb090a2716e5392fcba858a320c20ff5afd6f0
Katrinast <stehnovicekaterina@gmail.com>
2024-02-01 22:51:26 +0200

    added changins

--- js/modal.js
+++ js/modal.js
@@ -1,28 +1,32 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelectorAll('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+  const mobileMenu = document.querySelector('.mob-menu');
+  const openMenuBtn = document.querySelector('.header-open-btn');
+  const closeMenuBtn = document.querySelector('.mob-close-btn');
+    const closeMenuLink = document.querySelectorAll('.mob-nav-link');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
   };
+  closeMenuLink.forEach(item => item.addEventListener('click', toggleMenu));
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
-  refs.openModalBtn.forEach((element) => {
-    element.addEventListener('click', toggleModal);
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
   });
-  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  //* Закривання по кліку на бекдроп
-
-  refs.modal.addEventListener('click', removeModal);
-  function removeModal(e) {
-    if (e.target === e.currentTarget) {
-      refs.modal.classList.add('is-hidden');
-    }
-  }
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    document.body.classList.toggle('no-scroll');
-  }
   document.querySelector(".section-hero-button").addEventListener("click", toggleOrderServiceModal);
 	document.querySelector(".modal-close-btn").addEventListener("click", toggleOrderServiceModal);
 
